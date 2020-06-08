451c5c3d8d861323179c577c473e8bb9ffdc34b5
Den <fentau.line@gmail.com>
2020-06-09 01:10:13 +0300

    Updates

--- precache-manifest.d0b1028d170939b2a58d78d068371b70.js
+++ precache-manifest.d0b1028d170939b2a58d78d068371b70.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0c0827702b219d5e8eee662bcce11809",
+    "revision": "fb20bd9d04bccb759c5d9ce099b6508c",
     "url": "/DeltaShop/index.html"
   },
   {
-    "revision": "e474fddd0a2d7abec48f",
+    "revision": "8b9bba7778f386ab55ef",
     "url": "/DeltaShop/static/css/main.5c39fc79.chunk.css"
   },
   {
-    "revision": "3e99373ae70636c56ac0",
-    "url": "/DeltaShop/static/js/2.38f46e16.chunk.js"
+    "revision": "fbbd04d26306f64c210e",
+    "url": "/DeltaShop/static/js/2.f1e07918.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/DeltaShop/static/js/2.38f46e16.chunk.js.LICENSE.txt"
+    "url": "/DeltaShop/static/js/2.f1e07918.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e474fddd0a2d7abec48f",
-    "url": "/DeltaShop/static/js/main.70ffdc34.chunk.js"
+    "revision": "8b9bba7778f386ab55ef",
+    "url": "/DeltaShop/static/js/main.ff964b26.chunk.js"
   },
   {
     "revision": "bd9233e83b39a5b8182c",
